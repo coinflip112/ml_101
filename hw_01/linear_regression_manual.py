24855fb5d50b52a076b5216d5dde9b6a8cd765f7
Messa <smessa@kpmg.cz>
2019-10-08 18:54:00 +0200

    added first attempt to hw01 solution

--- hw_01/linear_regression_manual.py
+++ hw_01/linear_regression_manual.py
@@ -4,6 +4,11 @@ import argparse
 import numpy as np
 import sklearn.datasets
 import sklearn.model_selection
+from sklearn.metrics import mean_squared_error
+
+
+def rms(y_true, y_pred):
+    return np.sqrt(mean_squared_error(y_true, y_pred))
 
 
 if __name__ == "__main__":
@@ -19,17 +24,20 @@ if __name__ == "__main__":
 
     # TODO: Pad a value of `1` to every instance in dataset.data
     # (np.pad or np.concatenate might be useful).
-
+    X = dataset.data
+    y = dataset.target
     # TODO: Split data so that the last `args.test_size` data are the test
     # set and the rest is the training set.
-
+    X_train, X_test, y_train, y_test = sklearn.model_selection.train_test_split(
+        X, y, test_size=args.test_size
+    )
     # TODO: Solve the linear regression using the algorithm from the lecture,
     # explicitly computing the matrix inverse (using np.linalg.inv).
-
+    ols_weights = np.linalg.inv(X_train.T @ X_train) @ X_train.T @ y_train
     # TODO: Predict target values on the test set.
-
+    y_test_pred = X_test @ ols_weights
     # TODO: Compute root mean square error on the test set predictions.
-    rmse = None
+    rmse = rms(y_test, y_test_pred)
 
     with open("linear_regression_manual.out", "w") as output_file:
         print("{:.2f}".format(rmse), file=output_file)
