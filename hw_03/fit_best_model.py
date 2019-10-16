fc3c3c74c8fc7290876232514210ca661fb4c44d
Messa <smessa@kpmg.cz>
2019-10-16 18:42:49 +0200

    rewrote script to get accepted by recodex

--- hw_03/fit_best_model.py
+++ hw_03/fit_best_model.py
@@ -33,19 +33,53 @@ preprocessing = lambda features_mapping_dict: ColumnTransformer(
 binary_features = [1, 4]
 categorical_features = [0, 2, 3, 5, 6, 7]
 numerical_features = [8, 9, 10, 11]
-    
-    # create variable mapping dictionary
-features_mapping_dict = {
-        "numerics": numerical_features,
-        "binary": binary_features,
-        "categoricals": categorical_features,
-    }
 
-def fit_best_model(feature_mapping, best_params,save_to_disk = True)
+# create variable mapping dictionary
+features_mapping_dict = {
+    "numerics": numerical_features,
+    "binary": binary_features,
+    "categoricals": categorical_features,
+}
+
+best_params = {
+    "elastic_net": {"alpha": 0.013805212784019607, "l1_ratio": 0.9135450867220719},
+    "gb": {
+        "learning_rate": 0.051561333292395484,
+        "max_depth": 3,
+        "n_estimators": 996,
+        "subsample": 0.709693194180173,
+    },
+    "svr": {
+        "C": 14746.443911967926,
+        "degree": 4,
+        "gamma": 0.024061171791590563,
+        "kernel": "rbf",
+    },
+    "rf": {
+        "bootstrap": False,
+        "max_depth": 31,
+        "max_features": "log2",
+        "min_samples_leaf": 2,
+        "min_samples_split": 4,
+        "n_estimators": 98,
+    },
+}
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    "--model_path",
+    default="linear_regression_competition.model",
+    type=str,
+    help="Model path",
+)
+parser.add_argument("--seed", default=42, type=int, help="Random seed")
+
+
+def fit_best_model(
+    feature_mapping=features_mapping_dict, best_params=best_params, save_to_disk=True
+):
     # load data
     data = np.load("linear_regression_competition.train.npz")
     features, targets = data["data"], data["target"]
-
 
     # define models
     estimator_svr = Pipeline(
@@ -83,8 +117,23 @@ def fit_best_model(feature_mapping, best_params,save_to_disk = True)
     )
 
     voter.fit(features, targets)
-    
-    with open(args.model_path, "wb") as model_file:
+
+    with open("linear_regression_competition.model", "wb") as model_file:
         pickle.dump(voter, model_file)
 
-    
+
+def recodex_predict(data):
+    # The `data` is a Numpy array containt test se
+
+    with open("linear_regression_competition.model", "rb") as model_file:
+        model = pickle.load(model_file)
+
+    predictions = model.predict(data)
+    return predictions
+
+
+if __name__ == "__main__":
+    args = parser.parse_args()
+
+    np.random.seed(args.seed)
+
