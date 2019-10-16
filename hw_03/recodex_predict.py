098dfbb7e8f2c64109f4478e61cfe57c6a758443
Messa <smessa@kpmg.cz>
2019-10-16 18:43:11 +0200

    removed recodex predict script which did not work

--- hw_03/recodex_predict.py
+++
@@ -1,9 +0,0 @@
-import pickle
-def recodex_predict(data):
-    # The `data` is a Numpy array containt test se
-
-    with open("linear_regression_competition.model", "rb") as model_file:
-        model = pickle.load(model_file)
-
-    predictions = model.predict(data)
-    return predictions
