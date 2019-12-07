12bcc59b70e0c7cd8e28dfb44b7814590927823d
Samuel Messa <samo.messa@gmail.com>
2019-12-07 12:24:18 +0100

    added tests

---
+++ hw_16/run_tests.sh
@@ -0,0 +1,5 @@
+python decision_tree_classification.py --criterion=gini --max_depth=2 --seed=42
+python decision_tree_classification.py --criterion=gini --min_to_split=49 --seed=42
+python decision_tree_classification.py --criterion=gini --max_leaves=4 --seed=42
+python decision_tree_classification.py --criterion=entropy --max_leaves=5 --seed=42
+python decision_tree_classification.py --criterion=gini --max_depth=2 --seed=42
